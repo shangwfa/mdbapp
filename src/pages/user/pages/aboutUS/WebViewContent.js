6cab7c32590f4398ad41438b5504add3f2602305
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-12-10 10:53:19 +0800

    up

--- src/pages/user/pages/aboutUS/WebViewContent.js
+++ src/pages/user/pages/aboutUS/WebViewContent.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import {StyleSheet, ScrollView, Platform, Linking} from 'react-native';
-import {List} from '@ant-design/react-native';
+import {Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
 
-// function loadFlie(key) {
-//   const lang = this.props.lang === 'CN' ? 'CN' : 'US';
-//   const files = {
-//     ImportantStatement: {
-//       CN: '/webPage/ImportantStatementCN.html',
-//       US: '/webPage/ImportantStatementUS.html',
-//     },
-//   };
-//   const fileUri = files[key][lang];
-//   return Platform.OS === 'android'
-//     ? require(`.${fileUri}`)
-//     : {uri: `file:///android_asset${fileUri}`};
-// }
-
-// var source =
-//   Platform.OS === 'ios'
-//     ? require('./webPage/ImportantStatementCN.html')
-//     : {uri: 'file:///android_asset/webPage/ImportantStatementCN.html'};
-
-export default function WebViewContent({route, navigation}) {
-  console.log(121212, route, navigation);
-  return <WebView source={require('./webPage/ImportantStatementCN.html')} />;
+export default function WebViewContent({route, navigation, lang}) {
+  const loadFlie = () => {
+    const fileName = 'ImportantStatementCN';
+    const flie =
+      Platform.OS === 'android'
+        ? require(`./webPage/${fileName}.html`)
+        : {uri: `file:///android_asset/webPage/${fileName}.html`};
+    return flie;
+  };
+  return <WebView source={loadFlie('ImportantStatementCN')} />;
 }
