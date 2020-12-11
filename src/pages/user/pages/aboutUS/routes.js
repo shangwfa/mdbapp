003decd6561992a622aaf971c9be1726bd2df99a
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-12-11 10:37:44 +0800

    調整routes

---
+++ src/pages/user/pages/aboutUS/routes.js
@@ -0,0 +1,12 @@
+import AboutUS from './index';
+import WebViewContent from './WebViewContent';
+export default [
+  {
+    name: 'AboutUS',
+    component: AboutUS,
+  },
+  {
+    name: 'WebViewContent',
+    component: WebViewContent,
+  },
+];
---
+++ src/pages/user/pages/resetTransPin/routes.js
@@ -0,0 +1,7 @@
+import ResetTransPin from './index';
+export default [
+  {
+    name: 'ResetTransPin',
+    component: ResetTransPin,
+  },
+];
