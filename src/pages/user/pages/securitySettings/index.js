10c8470233370c9ceb9a2c519ba0e28a9a284ab2
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-12-16 14:47:12 +0800

    安全設置功能列表、移除文件

---
+++ src/pages/user/pages/securitySettings/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Linking} from 'react-native';
+import {List} from '@ant-design/react-native';
+import BasePage from '../../../BasePage';
+const Item = List.Item;
+class AboutUS extends BasePage {
+  constructor(props) {
+    super(props);
+    this.initHeader({
+      title: '安全設置',
+    });
+    const {navigation} = this.props;
+    this.state = {
+      itemArr: [
+        {
+          extra: 'www.mdb.com.mo',
+          onPress: () => {},
+          title: '修改登入密碼',
+        },
+        {
+          onPress: () => {},
+          title: '修改交易密碼',
+        },
+        {
+          onPress: () => {
+            navigation.navigate('TransPinVerifyCode', {
+              title: '获取重置交易密碼验证码',
+            });
+          },
+          title: '重置交易密碼',
+        },
+        {
+          onPress: () => {},
+          title: '指紋登入',
+          extra: '關閉',
+        },
+        {
+          onPress: () => {},
+          title: '快捷支付管理',
+        },
+        {
+          onPress: () => {},
+          title: '登入歷史',
+        },
+        {
+          onPress: () => {},
+          title: '登出設置',
+          extra: '2分鐘',
+        },
+      ],
+    };
+  }
+  renderContainer() {
+    const {itemArr} = this.state;
+    return (
+      <List>
+        {itemArr.map((item) => (
+          <Item {...item} key={item.title}>
+            {item.title}
+          </Item>
+        ))}
+      </List>
+    );
+  }
+}
+export default AboutUS;
