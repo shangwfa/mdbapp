37e77343da0f5ca38df91eb066f378a516089c85
xu.weiming <xu.weiming@mdb-fintech.com>
2020-12-23 16:27:21 +0800

    交易限額

---
+++ src/pages/user/pages/transLimit/LimitItem.js
@@ -0,0 +1,23 @@
+//交易限額組件
+import { View,Text } from 'react-native'
+import React from 'react'
+
+class LimitItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+    }
+  }
+  render() {
+    const {item} = this.props;
+    return (
+      <View>
+        <Text>{item.title}</Text>
+        <Text>單筆限額:{item.single_transaction_limit}</Text>
+        <Text>日累計限額:{item.daily_cumulative_limit}</Text>
+      </View>
+    )
+  }
+}
+
+export default LimitItem
