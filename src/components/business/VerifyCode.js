3cd82237f17a1fc807e6d553dfd1ce2b21e94f20
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-12-25 11:29:12 +0800

    修改交易限额

--- src/components/business/VerifyCode.js
+++ src/components/business/VerifyCode.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {StyleSheet, Button, Keyboard} from 'react-native';
-import {View, List, InputItem} from '@ant-design/react-native';
+import {StyleSheet, Keyboard} from 'react-native';
+import {View, List, InputItem, Button} from '@ant-design/react-native';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import CountDown from '#/components/base/CountDown';
@@ -12,7 +12,6 @@ class VerificationCode extends React.Component {
     this.state = {
       smsFlowNo: '',
       otp: '',
-      btnOtpDisabled: true,
       firstOnPress: true,
     };
   }
@@ -24,6 +23,7 @@ class VerificationCode extends React.Component {
       funcName: PropTypes.string,
     }),
     needCheckOtp: PropTypes.bool, //是否需要校驗短信驗證碼
+    showPhoneNum: PropTypes.bool,
   };
   static defaultProps = {
     httpData: {
@@ -31,6 +31,7 @@ class VerificationCode extends React.Component {
     },
     httpUrl: apiPaths.JSONURL,
     needCheckOtp: false,
+    showPhoneNum: true,
   };
   onPressOTPSend = async () => {
     //點擊發送或者重發
@@ -46,7 +47,6 @@ class VerificationCode extends React.Component {
       });
       this.setState({
         smsFlowNo: res.smsFlowNo,
-        btnOtpDisabled: false,
         firstOnPress: false,
       });
     } else {
@@ -62,9 +62,6 @@ class VerificationCode extends React.Component {
         ActionMethod: 'resendOtp',
         smsFlowNo: smsFlowNo,
       },
-    });
-    this.setState({
-      btnOtpDisabled: false,
     });
   };
 
@@ -90,14 +87,16 @@ class VerificationCode extends React.Component {
   };
 
   render() {
-    const {btnOtpDisabled} = this.state;
-    const {mobileNo} = this.props;
+    const {firstOnPress, otp} = this.state;
+    const {mobileNo, showPhoneNum} = this.props;
     return (
       <>
         <List>
-          <InputItem value={mobileNo}>電話號碼</InputItem>
+          {showPhoneNum ? (
+            <InputItem value={mobileNo}>電話號碼</InputItem>
+          ) : null}
           <InputItem
-            value={this.state.otp}
+            value={otp}
             onChange={(value) => {
               this.setState({
                 otp: value,
@@ -122,9 +121,10 @@ class VerificationCode extends React.Component {
               Keyboard.dismiss();
               this.submitVerifyCode();
             }}
-            disabled={btnOtpDisabled}
-            title="完成"
-          />
+            disabled={firstOnPress || !otp}
+            type="primary">
+            完成
+          </Button>
         </View>
       </>
     );
--- src/pages/paymentManagement/pages/merchantLimitSet/index.js
+++ src/pages/paymentManagement/pages/merchantLimitSet/index.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
-import {List, InputItem, Button} from '@ant-design/react-native';
+import {StyleSheet, ScrollView, Text} from 'react-native';
+import {List, InputItem, Button, Toast} from '@ant-design/react-native';
 import BasePage from '#/pages/BasePage';
 import RenderStaticPage from '#/components/render/RenderStaticPage';
-import {WhiteSpace} from '@ant-design/react-native';
+import VerifyCode from '#/components/business/VerifyCode';
 import HTTP from '#/api';
 import apiPaths from '../../paths/index';
 
@@ -44,27 +44,34 @@ class Index extends BasePage {
                   key: 'singleLimit',
                   extra: '',
                   suffixKey: 'ccy',
+                  type: 'number',
                 },
                 {
                   label: '日累计支付限额：',
                   key: 'dayLimit',
                   extra: '',
                   suffixKey: 'ccy',
+                  type: 'number',
                 },
               ],
             },
           },
           {
-            itemType: this.renderBtn,
+            itemType: VerifyCode,
             props: {
-              title: '完成',
-              key: '完成',
-              onPress: () => {},
+              httpData: {
+                funcName: 'app.mb.action.payment.PaymentManageAction.limit',
+              },
+              showPhoneNum: false,
+              submitVerifyCode: this.submitVerifyCode,
             },
           },
         ],
       },
-      merchantData: {},
+      merchantData: {
+        singleLimit: '',
+        dayLimit: '',
+      },
     };
   }
   didMount() {
@@ -81,7 +88,37 @@ class Index extends BasePage {
         bankCardNo: this.params.bankCardNo,
       },
     });
-    this.setState({merchantData});
+    this.setState({
+      merchantData: {
+        ...this.state.merchantData,
+        ...merchantData,
+      },
+    });
+  };
+  submitVerifyCode = async ({smsFlowNo, otp, firstOnPress}) => {
+    const {
+      merchantData: {singleLimit, dayLimit},
+    } = this.state;
+    if (singleLimit > dayLimit) {
+      Toast.info('单笔支付限额不可大于日累计支付限额');
+    }
+    const httpParams = {
+      ActionMethod: 'merchantLimitSet',
+      bankCardNo: this.params.bankCardNo,
+      merchantCode: this.params.merchantCode,
+      otp: otp,
+      exceedResendFlag: firstOnPress ? 'N' : 'Y',
+      exceedResend: firstOnPress ? 'N' : 'Y',
+      smsFlowNo: smsFlowNo,
+      singleLimit: singleLimit,
+      dayLimit: dayLimit,
+    };
+    await HTTP.api({
+      url: apiPaths.PAYMENT,
+      method: 'POST',
+      params: httpParams,
+    });
+    Toast.success('修改成功');
   };
   renderMerchantList = (props) => {
     const {merchantData} = this.state;
@@ -93,30 +130,28 @@ class Index extends BasePage {
     }));
     return (
       <List>
-        {merchantDes.map((item) =>
+        {merchantDes.map((item, index) =>
           item.suffixKey === 'ccy' ? (
             <InputItem
               {...item}
+              key={index}
               onChange={(value) => {
                 this.setState({
-                  [item.key]: value,
+                  merchantData: {
+                    ...merchantData,
+                    [item.key]: value,
+                  },
                 });
               }}>
               {item.label}
             </InputItem>
           ) : (
-            <Item {...item}>{item.label}</Item>
+            <Item {...item} key={index}>
+              {item.label}
+            </Item>
           ),
         )}
       </List>
-    );
-  };
-  renderBtn = (props) => {
-    return (
-      <>
-        <WhiteSpace size="sm" />
-        <Button type="primary">{props.title}</Button>
-      </>
     );
   };
   renderContainer() {
